7dd31094d57a6640933557b44efdb448068f9967
Wocked61 <DylanP@csu.fullerton.edu>
2024-10-30 19:32:43 -0700

    hello world

---
+++ README.md
@@ -0,0 +1,9 @@
+# Homework - Hello World
+
+My submission for Computer Science 131, Section 09, Lab Week 2
+
+# My Information
+
+* Name: Dylan Phan
+* CWID: 8855582536
+* Email: Dylanp@csu.fullerton.edu
---
+++ my_functions.cpp
@@ -0,0 +1,27 @@
+/**
+ * Complete this source code file
+ */
+
+/// YOUR WELCOME
+#include "my_functions.hpp"
+
+
+//
+#include <iostream>
+
+
+//
+using std::cout, std::endl;
+
+
+//
+int hello_world()
+{
+	//	TODO: Your code here
+	
+	cout << "Hello, world!";
+
+	return 23;
+}
+
+
